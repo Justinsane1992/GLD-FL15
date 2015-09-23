475fddfd08fee30f1a490e2f652be84026691064
Justinsane1992 <jgarner2@butlercc.edu>
2015-09-23 14:28:21 -0500

    assests
    upload assests

---
+++ Assets/Scripts/MoveTo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTo : MonoBehaviour {
+
+	public Transform goal;
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.destination = goal.position;
+	}
+}
